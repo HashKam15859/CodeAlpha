30e5ea0766b0e9f2b9c3ff206d33d67be4ad60c3
HashKam <157140391+HashKam15859@users.noreply.github.com>
2025-05-23 22:07:52 +0530

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,1 +1,36 @@
+import logging
+import threading
 
+_log_locks = {}
+
+
+def setup_logger(name, log_file=None, level=logging.INFO):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    if not logger.handlers:
+        formatter = logging.Formatter(
+            "%(asctime)s | %(name)s | %(levelname)s | %(message)s)"
+        )
+        if not logfile:
+            logfile = f"{name.lower()}.log"
+            
+        file_handler = logging.FileHandler(logfile)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+        # Log to console
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+
+        _log_locks[name] = threading.Lock()
+
+    return logger
+
+
+def safe_log(logger, msg, level="info"):
+    lock = _log_locks.get(logger.name, threading.Lock())
+    with lock:
+        log_func = getattr(logger, level, logger.info)
+        log_func(msg)
